f39186c69d63bcffef1e1d592e1e45b4f196e8a8
mohsin23 <mohsin2369@gmail.com>
2021-02-26 02:42:44 -0500

    added cors installation

--- bug_django/settings.py
+++ bug_django/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'bug',
     'django_extensions',
     'rest_framework',
+    'corsheaders',
 ]
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
@@ -52,11 +53,16 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'bug_django.urls'
